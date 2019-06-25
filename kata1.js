4e0fe976b79a2e4099fed47ee7f3633a3a8b2b2f
crazyArtur <artur.dbsz@gmail.com>
2019-06-25 21:34:30 +1000

    Work in progress

--- kata1.js
+++ kata1.js
@@ -1,10 +1,34 @@
+/*
+If .firstName or .lastName are changed, then .fullName should also be changed
+If .fullName is changed, then .firstName and .lastName should also be changed.
+*/
+
 function NamedOne(first, last) {
-    this.firstName = first;
-    this.lastName = last;
-    this.fullName = this.firstName + ' ' + this.lastName;
+    this.firstN = first;
+    this.lastN = last;
+
+    Object.defineProperties(this, {
+        "firstName": {
+            get: function() { return this.firstN; },
+            set: function(name) { this.firstN = name; }
+        },
+        "lastName": {
+            get: function() { return this.lastN; },
+            set: function(name) { this.lastN = name; }
+        },
+        "fullName": {
+            get: function() { return this.firstN + ' ' + this.lastN; },
+            set: function(name) { let parts = name.split('');
+                                this.firstN = parts[0];
+                                this.lastN = parts[1];
+                            }
+        }
+
+        });
+//    this.fullName = this.firstName + ' ' + this.lastName;
 }
 
-var namedOne = new NamedOne("Naomi","Wang");
+var namedOne = new NamedOne('Naomi', 'Wang');
 
 console.log(namedOne);
 
