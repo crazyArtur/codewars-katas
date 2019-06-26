90da4d9a74542e8191685720bb4021ffb105fdfa
crazyArtur <artur.dbsz@gmail.com>
2019-06-26 20:50:59 +1000

    Finished logic

--- kata2.js
+++ kata2.js
@@ -2,20 +2,25 @@
 // "Bouncing balls"
 
 function bouncingBall(h,  bounce,  window) {
-    if (h < 0 ||
-        bounce < 0 ||
-        bounce > 1 ||
-        window > h)
+    if(h < 0 ||
+       bounce < 0 ||
+       bounce > 1 ||
+       window > h)
         return -1;
 
     let rebounce = h * bounce;
     let timesBallWasSeen = 1;
 
-    while(rebounce > window){
-
+    while(rebounce >= window){
+        if(rebounce == window)
+            timesBallWasSeen++;
+        else
+            timesBallWasSeen += 2;
+        
+        rebounce *= bounce;
     }
     return timesBallWasSeen;
 }
 
 
-console.log(bouncingBall(0, 0.5, 1));
+console.log(bouncingBall(10, 0.5, 1));
