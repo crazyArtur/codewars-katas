e34438189a8bfa38271d15e71a4918bc3bfdb069
crazyArtur <artur.dbsz@gmail.com>
2019-06-26 20:10:27 +1000

    Added coinditions checking

---
+++ kata2.js
@@ -0,0 +1,11 @@
+// 6 kyu
+// "Bouncing balls"
+
+function bouncingBall(h,  bounce,  window) {
+    if (h < 0 ||
+        bounce < 0 ||
+        bounce > 1 ||
+        window > h)
+        return -1;
+
+  }
