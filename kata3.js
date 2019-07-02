36532f0d3d51c5fb501f1fe7116629188d34b449
crazyArtur <artur.dbsz@gmail.com>
2019-07-02 20:12:22 +1000

    Added new kata

---
+++ kata3.js
@@ -0,0 +1,8 @@
+/*
+6 kyu
+"Simple Encryption #1 - Alternating Split"
+
+For building the encrypted string:
+Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
+Do this n times!
+*/
