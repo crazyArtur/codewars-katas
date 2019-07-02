db215b51885e0be211696ed9fed95988ea9258c4
crazyArtur <artur.dbsz@gmail.com>
2019-07-02 20:25:48 +1000

    Basic logic

--- kata3.js
+++ kata3.js
@@ -6,3 +6,21 @@ For building the encrypted string:
 Take every 2nd char from the string, then the other chars, that are not every 2nd char, and concat them as new String.
 Do this n times!
 */
+
+function encrypt(text, n) {
+    if(text !== null && text != ''){
+
+    }
+    else
+        return text;
+
+}
+
+function decrypt(encryptedText, n) {
+    if(encryptedText !== null && encryptedText != ''){
+
+    }
+    else
+        return encryptedText;
+
+}
