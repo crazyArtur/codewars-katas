031753f79830b39f785dd3c4bb1376fda93878e9
crazyArtur <artur.dbsz@gmail.com>
2019-07-03 20:14:59 +1000

    Added logic

--- kata3.js
+++ kata3.js
@@ -8,20 +8,25 @@ Do this n times!
 */
 
 function encrypt(text, n) {
+    if(n <= 0)
+        return text;
+
     if(text !== null && text != ''){
         let sourceLength = text.length;
         let oddPart = '';
         let evenPart = '';
         for(let i = 0; i+1 < sourceLength; i+=2){
-
-
+            oddPart += text[i];
+            evenPart += text[i+1];
         }
         
-
+        if(sourceLength % 2 != 0)
+            oddPart += text[sourceLength-1];
+        
+        return encrypt(evenPart.concat(oddPart), n-1);
     }
     else
         return text;
-
 }
 
 function decrypt(encryptedText, n) {
