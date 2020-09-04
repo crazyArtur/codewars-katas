111911f0706729c144d3f28aae5654c118e94231
crazyArtur <artur.dbsz@gmail.com>
2020-09-04 16:36:30 +0200

    Solved puzzle + old file format with prettier

--- kata1.js
+++ kata1.js
@@ -4,33 +4,42 @@ If .fullName is changed, then .firstName and .lastName should also be changed.
 */
 
 function NamedOne(first, last) {
-    this.firstN = first;
-    this.lastN = last;
+  this.firstN = first;
+  this.lastN = last;
 
-    Object.defineProperties(this, {
-        "firstName": {
-            get: function() { return this.firstN; },
-            set: function(name) { this.firstN = name; }
-        },
-        "lastName": {
-            get: function() { return this.lastN; },
-            set: function(name) { this.lastN = name; }
-        },
-        "fullName": {
-            get: function() { return this.firstN + ' ' + this.lastN; },
-            set: function(name) { 
-                if (name.match(/[a-z]+ [a-z]/i)){
-                    let parts = name.split(' ');
-                        this.firstN = parts[0];
-                        this.lastN = parts[1];
-                }
-            }
+  Object.defineProperties(this, {
+    firstName: {
+      get: function () {
+        return this.firstN;
+      },
+      set: function (name) {
+        this.firstN = name;
+      },
+    },
+    lastName: {
+      get: function () {
+        return this.lastN;
+      },
+      set: function (name) {
+        this.lastN = name;
+      },
+    },
+    fullName: {
+      get: function () {
+        return this.firstN + " " + this.lastN;
+      },
+      set: function (name) {
+        if (name.match(/[a-z]+ [a-z]/i)) {
+          let parts = name.split(" ");
+          this.firstN = parts[0];
+          this.lastN = parts[1];
         }
-
-    });
+      },
+    },
+  });
 }
 
-var namedOne = new NamedOne('Naomi', 'Wang');
+var namedOne = new NamedOne("Naomi", "Wang");
 
 console.log(namedOne.firstName);
 console.log(namedOne.lastName);
--- kata5.js
+++ kata5.js
@@ -17,8 +17,46 @@ there are 10 matches in the championship
 0 <= x <= 4
 0 <= y <= 4
 */
+const games = [
+  "3:1",
+  "2:2",
+  "0:1",
+  "3:1",
+  "2:2",
+  "0:1",
+  "3:1",
+  "2:2",
+  "0:1",
+  "1:1",
+];
 
 function points(games) {
-  // your code here
+  var points = 0;
+  for (let i = 0; i < 10; i++) {
+    let x = games[i].split(":")[0];
+    let y = games[i].split(":")[1];
+    if (x > y) points += 3;
+    if (x == y) points++;
+  }
+  console.log(parseInt(points));
+
   parseInt(games, 10);
 }
+
+var points = 0;
+for (let i = 0; i < 10; i++) {
+  if (games[i].split(":")[0] > games[i].split(":")[1]) points += 3;
+  if (games[i].split(":")[0] == games[i].split(":")[1]) points++;
+}
+console.log(points);
+
+var a = games[0].split(":");
+
+console.log(parseInt("0xF"));
+console.log(parseInt("015", 8));
+// console.log(a[0]);
+// console.log("Length = ", games.length);
+
+// var b = a[0] - a[1];
+
+// console.log(b);
