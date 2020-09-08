dd45da0235ba6598b58e879036a155740c7668d4
crazyArtur <artur.dbsz@gmail.com>
2020-09-08 23:31:20 +0200

    kata8 - wip

--- kata8.js
+++ kata8.js
@@ -1,9 +1,8 @@
 /*
 7 kyu
 
-Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
-
-each taken only once - coming from s1 or s2.
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, 
+containing distinct letters, each taken only once - coming from s1 or s2.
 
 EXAMPLES
 a = "xyaabbbccccdefww"
@@ -13,3 +12,7 @@ longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 */
+
+function longest(s1, s2) {
+  // your code
+}
